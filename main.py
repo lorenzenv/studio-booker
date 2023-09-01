9367d4b2b0754f2bdd7f148737f5cd3dacb21010
Valentin Lorenzen <valentinlorenzen@mailbox.org>
2023-09-01 12:47:24 +0200

    aider: Added 'Date' column to the booking_status DataFrame and removed the index before displaying it.
    # Aider chat conversation:
    
    ASSISTANT: To add the date to the displayed DataFrame, we need to modify the `main.py` file. We will add a new column 'Date' to the `booking_status` DataFrame and fill it with the index values (which are the dates).
    
    Here is the *edit block* for the change:
    
    ```python
    main.py
    <<<<<<< HEAD
    # Create a DataFrame for the next 14 days
    dates = pd.date_range(start=pd.Timestamp.today(), periods=14)
    booking_status = pd.DataFrame(index=dates, columns=['Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']).fillna(False)
    
    # Update the DataFrame with the booking information when the form is submitted
    if submit_button:
        new_booking = {'Band Name': band_name, 'Booking Date': booking_date, 'Booking Time': booking_time}
        booking_data = booking_data.append(new_booking, ignore_index=True)
        booking_status.loc[booking_date, booking_time] = True
    
    # Display the booking status
    AgGrid(booking_status)
    =======
    # Create a DataFrame for the next 14 days
    dates = pd.date_range(start=pd.Timestamp.today(), periods=14)
    booking_status = pd.DataFrame(index=dates, columns=['Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']).fillna(False)
    booking_status['Date'] = booking_status.index
    
    # Update the DataFrame with the booking information when the form is submitted
    if submit_button:
        new_booking = {'Band Name': band_name, 'Booking Date': booking_date, 'Booking Time': booking_time}
        booking_data = booking_data.append(new_booking, ignore_index=True)
        booking_status.loc[booking_date, booking_time] = True
    
    # Display the booking status
    AgGrid(booking_status.reset_index(drop=True))
    >>>>>>> updated
    ```
    
    This change will add a new column 'Date' to the `booking_status` DataFrame and fill it with the index values (which are the dates). The `reset_index(drop=True)` function is used to remove the index from the DataFrame before displaying it.

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ with st.form('Booking Form'):
 # Create a DataFrame for the next 14 days
 dates = pd.date_range(start=pd.Timestamp.today(), periods=14)
 booking_status = pd.DataFrame(index=dates, columns=['Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']).fillna(False)
+booking_status['Date'] = booking_status.index
 
 # Update the DataFrame with the booking information when the form is submitted
 if submit_button:
@@ -33,4 +34,4 @@ if submit_button:
     booking_status.loc[booking_date, booking_time] = True
 
 # Display the booking status
-AgGrid(booking_status)
+AgGrid(booking_status.reset_index(drop=True))
