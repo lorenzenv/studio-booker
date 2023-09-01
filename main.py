59b603cb5546c626c8ce107756f3968167a567e9
Valentin Lorenzen <valentinlorenzen@mailbox.org>
2023-09-01 15:57:02 +0200

    Refactored the code to move the band name input and date selection to the top of the form.

--- main.py
+++ main.py
@@ -74,21 +74,21 @@ def form():
             st.session_state['booking_data'] = st.session_state['booking_data'].append(new_booking, ignore_index=True)
             update_booking_times(st.session_state['booking_data'])
 
+band_name = st.text_input('Band Name')
+min_date = pd.Timestamp.today()
+max_date = min_date + pd.DateOffset(days=14)
+if 'booking_date' not in st.session_state:
+    st.session_state['booking_date'] = min_date
+if 'booking_date_input' not in st.session_state:
+    st.session_state['booking_date_input'] = st.session_state['booking_date']
+st.session_state['booking_date_input'] = st.date_input('Booking Date', value=st.session_state['booking_date_input'], min_value=min_date, max_value=max_date, format="DD.MM.YYYY", on_change=update_available_times, args=[st.session_state['booking_date_input']])
+available_times = st.session_state['available_times']
+if available_times:
+    booking_time = st.selectbox('Booking Time', available_times)
+else:
+    st.info('No available times for this date.')
 column1, column2 = st.columns(2)
 with column1:
-    band_name = st.text_input('Band Name')
-    min_date = pd.Timestamp.today()
-    max_date = min_date + pd.DateOffset(days=14)
-    if 'booking_date' not in st.session_state:
-        st.session_state['booking_date'] = min_date
-    if 'booking_date_input' not in st.session_state:
-        st.session_state['booking_date_input'] = st.session_state['booking_date']
-    st.session_state['booking_date_input'] = st.date_input('Booking Date', value=st.session_state['booking_date_input'], min_value=min_date, max_value=max_date, format="DD.MM.YYYY", on_change=update_available_times, args=[st.session_state['booking_date_input']])
-    available_times = st.session_state['available_times']
-    if available_times:
-        booking_time = st.selectbox('Booking Time', available_times)
-    else:
-        st.info('No available times for this date.')
     form()
 
 dates = pd.date_range(start=pd.Timestamp.today(), periods=14)
