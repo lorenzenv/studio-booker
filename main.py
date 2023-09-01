717f6c6363e5d002fbedf8c76ed4689723246080
Valentin Lorenzen <valentinlorenzen@mailbox.org>
2023-09-01 12:58:23 +0200

    aider: Replace `emoji.emojize(':red_circle:')` and `emoji.emojize(':green_circle:')` with Streamlit's built-in emojis.
    # Aider chat conversation:
    
    USER: ModuleNotFoundError: No module named 'emoji'... can we not just use streamlit emojis ?
    
    ASSISTANT: Yes, we can use Streamlit's built-in emojis instead of the `emoji` library. We will need to replace the `emoji.emojize(':red_circle:')` and `emoji.emojize(':green_circle:')` with Streamlit's built-in emojis.
    
    Here is the change that needs to be made:
    
    ```python
    main.py
    <<<<<<< HEAD
    import emoji
    ...
    booking_status = pd.DataFrame(index=dates, columns=['Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']).fillna(emoji.emojize(':red_circle:'))
    ...
    booking_status.loc[booking_status['Date'] == booking_date_str, booking_time] = emoji.emojize(':green_circle:')
    =======
    ...
    booking_status = pd.DataFrame(index=dates, columns=['Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']).fillna(':red_circle:')
    ...
    booking_status.loc[booking_status['Date'] == booking_date_str, booking_time] = ':green_circle:'
    >>>>>>> updated
    ```
    
    This change will replace the `emoji` library with Streamlit's built-in emojis. The red and green circle emojis are represented as `:red_circle:` and `:green_circle:` respectively in Streamlit.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # Import necessary libraries
 import streamlit as st
 import pandas as pd
-import emoji
 from st_aggrid import AgGrid
 from streamlit_calendar import calendar
 
@@ -27,7 +26,7 @@ with st.form('Booking Form'):
 
 # Create a DataFrame for the next 14 days
 dates = pd.date_range(start=pd.Timestamp.today(), periods=14)
-booking_status = pd.DataFrame(index=dates, columns=['Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']).fillna(emoji.emojize(':red_circle:'))
+booking_status = pd.DataFrame(index=dates, columns=['Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']).fillna(':red_circle:')
 booking_status['Date'] = booking_status.index.strftime('%d.%m.%Y')
 
 # Update the DataFrame with the booking information when the form is submitted
@@ -35,7 +34,7 @@ if submit_button:
     new_booking = {'Band Name': band_name, 'Booking Date': booking_date, 'Booking Time': booking_time}
     booking_data = booking_data.append(new_booking, ignore_index=True)
     booking_date_str = booking_date.strftime('%d.%m.%Y')
-    booking_status.loc[booking_status['Date'] == booking_date_str, booking_time] = emoji.emojize(':green_circle:')
+    booking_status.loc[booking_status['Date'] == booking_date_str, booking_time] = ':green_circle:'
 
 # Display the booking status
 AgGrid(booking_status.reset_index(drop=True))
