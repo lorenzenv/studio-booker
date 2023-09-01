86ae973adeaaa6e3475e180b1b40bf96e56c477b
Valentin Lorenzen <valentinlorenzen@mailbox.org>
2023-09-01 15:51:21 +0200

    aider: Initialize `st.session_state['booking_date_input']` before it's used in the `st.date_input` widget.
    # Aider chat conversation:
    
    ASSISTANT: It seems like the error is due to the `st.session_state['booking_date_input']` not being initialized before it's used. We need to ensure that `st.session_state['booking_date_input']` is initialized before it's used in the `st.date_input` widget.
    
    Here is the change that needs to be made:
    
    ```python
    main.py
    <<<<<<< HEAD
        if 'booking_date' not in st.session_state:
            st.session_state['booking_date'] = min_date
        st.session_state['booking_date_input'] = st.date_input('Booking Date', value=st.session_state['booking_date'], min_value=min_date, max_value=max_date, format="DD.MM.YYYY", on_change=update_available_times, args=[st.session_state['booking_date_input']])
    =======
        if 'booking_date' not in st.session_state:
            st.session_state['booking_date'] = min_date
        if 'booking_date_input' not in st.session_state:
            st.session_state['booking_date_input'] = st.session_state['booking_date']
        st.session_state['booking_date_input'] = st.date_input('Booking Date', value=st.session_state['booking_date_input'], min_value=min_date, max_value=max_date, format="DD.MM.YYYY", on_change=update_available_times, args=[st.session_state['booking_date_input']])
    >>>>>>> updated
    ```
    
    This change initializes `st.session_state['booking_date_input']` before it's used in the `st.date_input` widget.

--- main.py
+++ main.py
@@ -78,7 +78,9 @@ with column1:
     max_date = min_date + pd.DateOffset(days=14)
     if 'booking_date' not in st.session_state:
         st.session_state['booking_date'] = min_date
-    st.session_state['booking_date_input'] = st.date_input('Booking Date', value=st.session_state['booking_date'], min_value=min_date, max_value=max_date, format="DD.MM.YYYY", on_change=update_available_times, args=[st.session_state['booking_date_input']])
+    if 'booking_date_input' not in st.session_state:
+        st.session_state['booking_date_input'] = st.session_state['booking_date']
+    st.session_state['booking_date_input'] = st.date_input('Booking Date', value=st.session_state['booking_date_input'], min_value=min_date, max_value=max_date, format="DD.MM.YYYY", on_change=update_available_times, args=[st.session_state['booking_date_input']])
     available_times = st.session_state['available_times']
     if available_times:
         booking_time = st.selectbox('Booking Time', available_times)
