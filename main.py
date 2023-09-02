968f8ef066191cb7c6d74eea6223e943312744f5
Valentin Lorenzen <valentinlorenzen@mailbox.org>
2023-09-02 16:00:48 +0200

    first commit

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from st_aggrid import AgGrid
 from streamlit_calendar import calendar
 from st_files_connection import FilesConnection
 import boto3
+from io import StringIO
 from datetime import datetime
 
-st.title('Band Studio Booking')
+st.set_page_config(page_title="Liebermann Studio Zeit", layout="wide", page_icon=":guitar:")
+
+st.title(':guitar: Liebermann Studio Zeit')
 
 conn = st.experimental_connection('s3', type=FilesConnection)
 
@@ -19,105 +22,165 @@ session = boto3.Session(
 
 s3 = session.resource('s3')
 
-def update_booking_times(booking_data):
-    booking_data.to_csv("booking_times.csv", index=False)
-    s3.Bucket('studio-booker').upload_file("booking_times.csv", "booking_times.csv")
 
-def update_available_times(date):
-    if date is not None:
-        formatted_date = convert_date_format(date)
-        st.session_state['available_times'] = get_available_times(formatted_date)
+bucket_name = "studio-booker"  
+file_key = "booking_times.csv"
 
-def get_booking_status(booking_data):
-    dates = pd.date_range(start=pd.Timestamp.today(), periods=14)
-    booking_status = pd.DataFrame(index=dates, columns=['Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']).fillna('🟢')
-    booking_status['Date'] = booking_status.index.strftime('%d.%m.%Y')
-    for index, row in st.session_state['booking_data'].iterrows():
-        booking_date_str = row['Booking Date'].strftime('%d.%m.%Y')
-        booking_status.loc[booking_status['Date'] == booking_date_str, row['Booking Time']] = '🔴 - ' + row['Band Name']
+######################
+### UTIL FUNCTIONS ###
+######################
 
-    return booking_status
+def get_df():
+    obj = s3.Object(bucket_name, file_key)
+    response = obj.get()
+    read_buffer = StringIO(response['Body'].read().decode())
+    df = pd.read_csv(read_buffer)
+    return df
 
-def get_available_times(date):
+def get_available_times(date, df):
     times = ['Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']
-    bookings_on_date = st.session_state['booking_data'][st.session_state['booking_data']['Booking Date'] == date]
+    date = convert_date_format(date)
+    bookings_on_date = df[df['Booking Date'] == date]
     for index, booking in bookings_on_date.iterrows():
         if booking['Booking Time'] in times:
             times.remove(booking['Booking Time'])
     return times
 
 def convert_date_format(input_date):
-    output_date = str(input_date) + " 00:00:00"
-    
-    return output_date
+    return input_date.strftime('%d.%m.%Y')
 
-if 'booking_data' not in st.session_state:
-    try:
-        st.session_state['booking_data'] = pd.read_csv("booking_times.csv")
-        st.session_state['booking_data']['Booking Date'] = pd.to_datetime(st.session_state['booking_data']['Booking Date'])
-    except FileNotFoundError:
-        st.session_state['booking_data'] = pd.DataFrame(columns=['Band Name', 'Booking Date', 'Booking Time'])
-        st.session_state['booking_data'].to_csv("booking_times.csv", index=False)
-        s3.Bucket('studio-booker').upload_file("booking_times.csv", "booking_times.csv")
-st.session_state['booking_data']
+def update_booking_times(band_name, datum, selected_time):
+    booking_data = pd.DataFrame({'Band Name': [band_name], 'Booking Date': [datum], 'Booking Time': [selected_time]})
+    df = get_df()
+    df = df.append(booking_data)
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, index=False)
 
-if 'available_times' not in st.session_state:
-    st.session_state['available_times'] = []
+    s3.Object(bucket_name, file_key).put(Body=csv_buffer.getvalue())
+    st.info("**Buchung erfolgreich!**")
 
+def get_booking_status(df):
+    dates = pd.date_range(start=pd.Timestamp.today(), periods=14)
+    booking_status = pd.DataFrame(index=dates, columns=['Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']).fillna('🟢')
+    booking_status['Date'] = booking_status.index.strftime('%d.%m.%Y')
+    for index, row in df.iterrows():
+        booking_date_str = row['Booking Date']
+        booking_status.loc[booking_status['Date'] == booking_date_str, row['Booking Time']] = '🔴 - ' + row['Band Name']
 
+    booking_status = booking_status.reindex(columns=['Date', 'Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)'])  # Replace these with the actual column names
+    return booking_status
 
-def form():
-    with st.form('Booking Form'):
-        submit_button = st.form_submit_button('Book Now')
+def get_all_bookings(df):
+    df = df.sort_values(by=['Booking Date', 'Booking Time'])
+    formatted_list = []
+    for index, row in df.iterrows():
+        formatted_string = f"{row['Booking Date']} - {row['Band Name']} - {row['Booking Time']}"
+        formatted_list.append(formatted_string)
+    return formatted_list
 
-        if submit_button:
-            new_booking = {'Band Name': band_name, 'Booking Date': st.session_state['booking_date'], 'Booking Time': booking_time}
-            st.session_state['booking_data'] = st.session_state['booking_data'].append(new_booking, ignore_index=True)
-            update_booking_times(st.session_state['booking_data'])
+def delete_booking(booking_to_delete, df):
+    indices_to_remove = []
 
-band_name = st.text_input('Band Name')
+    for index, row in df.iterrows():
+        formatted_string = f"{row['Booking Date']} - {row['Band Name']} - {row['Booking Time']}"
+        if formatted_string == booking_to_delete:
+            indices_to_remove.append(index)
+
+    df.drop(indices_to_remove, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, index=False)
+    s3.Object(bucket_name, file_key).put(Body=csv_buffer.getvalue())
+
+######################
+###### MAIN PAGE #####
+######################
+
+df = get_df()
+
 min_date = pd.Timestamp.today()
 max_date = min_date + pd.DateOffset(days=14)
-if 'booking_date' not in st.session_state:
-    st.session_state['booking_date'] = min_date
-if 'booking_date_input' not in st.session_state:
-    st.session_state['booking_date_input'] = st.session_state['booking_date']
-st.session_state['new_booking_date_input'] = st.date_input('Booking Date', value=st.session_state['booking_date_input'], min_value=min_date, max_value=max_date, format="DD.MM.YYYY", on_change=update_available_times, args=[st.session_state['new_booking_date_input']])
-if 'available_times' in st.session_state and st.session_state['available_times']:
-    booking_time = st.selectbox('Booking Time', st.session_state['available_times'])
-else:
-    st.info('No available times for this date.')
+
 column1, column2 = st.columns(2)
+
 with column1:
-    form()
+    band_name = st.text_input('Wer?')
+    datum = st.date_input(min_value=min_date, max_value=max_date, label='Welcher Tag?', format="DD.MM.YYYY")
+    datum_formatted = convert_date_format(datum)
+    available_times = get_available_times(datum, df)
+    if available_times != []:
+        selected_time = st.selectbox('Welche Uhrzeit?', available_times)
+        send_button = st.button('Buchen')
+    else:
+        send_button = st.button("Keine Zeit mehr frei an diesem Tag", disabled=True)
 
-dates = pd.date_range(start=pd.Timestamp.today(), periods=14)
-booking_status = pd.DataFrame(index=dates, columns=['Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']).fillna('🟢')
-booking_status['Date'] = booking_status.index.strftime('%d.%m.%Y')
-booking_status = booking_status[['Date', 'Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']]
+    if send_button:
+        st.info(f"**{datum_formatted} - {selected_time}** \n\n für **{band_name}** gebucht")
+        update_booking_times(band_name, datum_formatted, selected_time)
+        st.experimental_rerun()
 
-for index, row in st.session_state['booking_data'].iterrows():
-    booking_date_str = row['Booking Date'].strftime('%d.%m.%Y')
-    booking_status.loc[booking_status['Date'] == booking_date_str, row['Booking Time']] = '🔴 - ' + row['Band Name']
+    all_bookings = get_all_bookings(df)
+    booking_to_delete = st.selectbox("Buchungen", all_bookings)
+
+    delete_button = st.button("Löschen")
+    if delete_button:
+        delete_booking(booking_to_delete, df)
+        st.experimental_rerun()
+
+booking_status = get_booking_status(df)
 
 with column2:
-    with st.form('Remove Booking Form'):
-        bookings = st.session_state['booking_data'].apply(lambda row: f"{row['Band Name']} - {row['Booking Date'].strftime('%d.%m.%Y')} - {row['Booking Time']}", axis=1).values.tolist()
-        selected_booking = st.selectbox('Select a booking to remove', bookings)
-        remove_button = st.form_submit_button('Remove Booking')
+    st.dataframe(booking_status, use_container_width=True, hide_index=True, height=528)
 
-        if remove_button:
-            if selected_booking is not None:
-                band_name, booking_date_str, booking_time = selected_booking.split(' - ')
-                booking_date = pd.to_datetime(booking_date_str, format='%d.%m.%Y')
-                booking_to_remove = st.session_state['booking_data'][(st.session_state['booking_data']['Band Name'] == band_name) & (st.session_state['booking_data']['Booking Date'] == booking_date) & (st.session_state['booking_data']['Booking Time'] == booking_time)]
-                st.session_state['booking_data'] = st.session_state['booking_data'].drop(booking_to_remove.index)
-                update_booking_times(st.session_state['booking_data'])
-                booking_status = get_booking_status(st.session_state['booking_data'])
-            else:
-                st.warning('No booking selected.')
+# def update_booking_times(booking_data):
+#     booking_data.to_csv("booking_times.csv", index=False)
+#     s3.Bucket('studio-booker').upload_file("booking_times.csv", "booking_times.csv")
 
-AgGrid(booking_status.reset_index(drop=True))
-def update_available_times(date):
-    formatted_date = convert_date_format(date)
-    st.session_state['available_times'] = get_available_times(formatted_date)
+# def update_available_times(date):
+#     if date is not None:
+#         formatted_date = convert_date_format(date)
+#         st.session_state['available_times'] = get_available_times(formatted_date)
+
+# def get_available_times(date):
+#     times = ['Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']
+#     bookings_on_date = st.session_state['booking_data'][st.session_state['booking_data']['Booking Date'] == date]
+#     for index, booking in bookings_on_date.iterrows():
+#         if booking['Booking Time'] in times:
+#             times.remove(booking['Booking Time'])
+#     return times
+
+# def convert_date_format(input_date):
+#     output_date = str(input_date) + " 00:00:00"
+    
+#     return output_date
+
+# band_name = st.text_input('Band Name')
+# min_date = pd.Timestamp.today()
+# max_date = min_date + pd.DateOffset(days=14)
+
+# submit_button = st.button('Book Now')
+
+# if submit_button:
+#     new_booking = {'Band Name': band_name, 'Booking Date': booking_date, 'Booking Time': booking_time}
+
+# dates = pd.date_range(start=pd.Timestamp.today(), periods=14)
+# booking_status = pd.DataFrame(index=dates, columns=['Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']).fillna('🟢')
+# booking_status['Date'] = booking_status.index.strftime('%d.%m.%Y')
+# booking_status = booking_status[['Date', 'Tagsüber (bis 19 Uhr)', 'Abends (ab 19 Uhr)']]
+
+# for index, row in st.session_state['booking_data'].iterrows():
+#     booking_date_str = row['Booking Date'].strftime('%d.%m.%Y')
+#     booking_status.loc[booking_status['Date'] == booking_date_str, row['Booking Time']] = '🔴 - ' + row['Band Name']
+
+# bookings = st.session_state['booking_data'].apply(lambda row: f"{row['Band Name']} - {row['Booking Date'].strftime('%d.%m.%Y')} - {row['Booking Time']}", axis=1).values.tolist()
+# selected_booking = st.selectbox('Select a booking to remove', bookings)
+# remove_button = st.button('Remove Booking')
+
+# if remove_button:
+#     if selected_booking is not None:
+#         band_name, booking_date_str, booking_time = selected_booking.split(' - ')
+#         booking_date = pd.to_datetime(booking_date_str, format='%d.%m.%Y')
+#     else:
+#         st.warning('No booking selected.')
+
+# booking_status.reset_index(drop=True)
